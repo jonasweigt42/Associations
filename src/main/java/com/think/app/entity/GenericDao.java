1f386c8586349dffda628b9c7d88ebded86ae0b4
Jonas Weigt <jonas.weigt42@gmail.com>
2020-09-13 22:00:08 +0200

    changed method to subclass

--- src/main/java/com/think/app/entity/GenericDao.java
+++ src/main/java/com/think/app/entity/GenericDao.java
@@ -1,13 +1,10 @@
 package com.think.app.entity;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,38 +35,6 @@ public abstract class GenericDao<T extends Serializable>
 	{
 		return ((Number) entityManager.createQuery("SELECT COUNT(c) FROM " + clazz.getName() + " c").getSingleResult())
 				.intValue();
-	}
-
-	@Transactional
-	public List<T> getRandomEntries(int number)
-	{
-		List<T> list = new ArrayList<>(number);
-		int countedObjects = count();
-		if(countedObjects <= number)
-		{
-			return findAll();
-		}
-		prepareListWithRandomElements(number, list, countedObjects);
-		return list;
-
-	}
-
-	@SuppressWarnings("unchecked")
-	private void prepareListWithRandomElements(int number, List<T> list, int countedObjects)
-	{
-		Random random = new Random();
-		while (list.size() != number)
-		{
-			int randomNumber = random.nextInt(countedObjects);
-			Query selectQuery = entityManager.createQuery("SELECT c FROM " + clazz.getName() + " c");
-			selectQuery.setFirstResult(randomNumber);
-			selectQuery.setMaxResults(1);
-			T result = (T) selectQuery.getSingleResult();
-			if (!list.contains(result))
-			{
-				list.add(result);
-			}
-		}
 	}
 
 	@Transactional
--- src/main/java/com/think/app/entity/word/WordDao.java
+++ src/main/java/com/think/app/entity/word/WordDao.java
@@ -1,8 +1,14 @@
 package com.think.app.entity.word;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import javax.annotation.PostConstruct;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.think.app.entity.GenericDao;
 
@@ -15,4 +21,36 @@ public class WordDao extends GenericDao<Word>
 	{
 		setClazz(Word.class);
 	}
+	
+	@Transactional
+	public List<Word> getRandomEntries(int number)
+	{
+		List<Word> list = new ArrayList<>(number);
+		int countedObjects = count();
+		if(countedObjects <= number)
+		{
+			return findAll();
+		}
+		prepareListWithRandomElements(number, list, countedObjects);
+		return list;
+
+	}
+
+	private void prepareListWithRandomElements(int number, List<Word> list, int countedObjects)
+	{
+		Random random = new Random();
+		while (list.size() != number)
+		{
+			int randomNumber = random.nextInt(countedObjects);
+			Query selectQuery = entityManager.createQuery("SELECT c FROM " + Word.class.getName() + " c");
+			selectQuery.setFirstResult(randomNumber);
+			selectQuery.setMaxResults(1);
+			Word result = (Word) selectQuery.getSingleResult();
+			if (!list.contains(result))
+			{
+				list.add(result);
+			}
+		}
+	}
+
 }
