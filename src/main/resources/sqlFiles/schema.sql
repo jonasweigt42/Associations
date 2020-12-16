bd4a7a931fa8871293c5db0386b48ccf5509903b
Jonas Weigt <jonas.weigt42@gmail.com>
2020-12-16 13:32:39 +0100

    fixed datastructure

--- src/main/resources/sqlFiles/schema.sql
+++ src/main/resources/sqlFiles/schema.sql
@@ -8,13 +8,15 @@ CREATE TABLE USER (
   last_name VARCHAR(250) NOT NULL,
   mail_address VARCHAR(250) NOT NULL,
   password VARCHAR(250) NOT NULL,
-  language VARCHAR(2) NOT NULL
+  language VARCHAR(2) NOT NULL,
+  UNIQUE KEY user_constraint (mail_address)
 );
  
 CREATE TABLE WORD (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(250) NOT NULL,
-  language VARCHAR(2) NOT NULL
+  language VARCHAR(2) NOT NULL,
+  UNIQUE KEY word_constraint (name, language)
 );
 
 CREATE TABLE ASSOCIATION (
@@ -24,5 +26,6 @@ CREATE TABLE ASSOCIATION (
  association VARCHAR(250) NOT NULL,
  association_date DATE NOT NULL,
  FOREIGN KEY (user_id) REFERENCES USER(id),
- FOREIGN KEY (word_id) REFERENCES WORD(id)
+ FOREIGN KEY (word_id) REFERENCES WORD(id),
+ UNIQUE KEY association_constraint (user_id, word_id, association)
  );
